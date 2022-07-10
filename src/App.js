9f58cb18735a57a0074701976c07a555bfaef90a
Tkachenko Anna <hanna.tkachenko13@gmail.com>
2022-07-10 22:46:53 +0300

    Add Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React from "react";
+import Header from "./components/Header/index";
+import {styles} from "./App.module.scss"
 
-
-
-function App() {
-  return 
-    }
+const App = () => {
+  return <Header />;
+};
 
 export default App;
