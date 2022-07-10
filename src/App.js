4d243e8ce5fb38e3270690f9c1efc064fb0a503f
Tkachenko Anna <hanna.tkachenko13@gmail.com>
2022-07-11 01:01:02 +0300

    Add SignUp/Login button

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import Header from "./components/Header/";
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
-
-
-
+import './App.module.scss'
 
 const App = () => {
   return (
-    <>
+   
       <BrowserRouter>
         <Header />
         <Routes>
@@ -19,7 +17,7 @@ const App = () => {
           <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
-    </>
+    
   );
 };
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,24 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const { pathname } = useLocation();
+
   return (
-    
-      <header>
-        <Link to="/">
-          <img src="/static/images/logo.png" alt="logo" />
-        </Link>
-      </header>
-    
+    <header>
+      <Link to="/">
+        <img src="/static/images/logo.png" alt="logo" />
+      </Link>
+
+      <button>
+        {pathname === "/login" ? (
+          <Link to="/signup">sign up</Link>
+        ) : (
+          <Link to="/login">login</Link>
+        )}
+      </button>
+    </header>
   );
 };
 
