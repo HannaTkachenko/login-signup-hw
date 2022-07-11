04465878f61f8a8544397f0447ee4fb61f4211e1
Tkachenko Anna <hanna.tkachenko13@gmail.com>
2022-07-11 04:30:09 +0300

    Add SignUpForm with styles, without radioinputs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,7 +6,7 @@ import {styles} from "./../common/styles/styles.scss"
 const Login = () => {
   return (
     <>
-      <h1 className="title-login">login to your account</h1>
+      <h1 className="title">login to your account</h1>
       <LoginForm />
     </>
   );
